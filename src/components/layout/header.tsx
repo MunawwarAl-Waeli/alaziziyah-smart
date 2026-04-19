7208e222a7e9add63f42099c1aaa0080cd540110
mun712326-art <mun712326@gmail.com>
2026-04-19 23:27:21 +0300

    file updategjيسيwqwewيشسيشسيشسياللب

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -281,7 +281,7 @@ export function Header({
     <>
       <motion.header
         className={cn(
-          "fixed top-0 left-0 right-0 z-50 transition-all duration-500",
+          "fixed top-0 left-0 right-0 z-500 transition-all duration-500",
           isScrolled
             ? "bg-background/85 backdrop-blur-2xl border-b border-primary/20 shadow-luxury py-3"
             : "bg-transparent py-5 border-b border-transparent",
